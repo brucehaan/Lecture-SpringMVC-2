074c283de0ff0df8bbd8f24cac46bb26190b0b92
brucehan <intragerhan@gmail.com>
2024-06-08 19:33:13 +0900

    Project Setting

---
+++ basic-thymeleaf/src/main/java/study/thymeleaf/BasicThymeleafApplication.java
@@ -0,0 +1,13 @@
+package study.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicThymeleafApplication.class, args);
+    }
+
+}
