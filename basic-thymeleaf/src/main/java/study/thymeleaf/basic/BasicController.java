ad5f9e64fc8bfed388391dbf29ee70f5ce8d1b51
brucehan <intragerhan@gmail.com>
2024-06-10 22:26:53 +0900

    Variable - SpringEL

--- basic-thymeleaf/src/main/java/study/thymeleaf/basic/BasicController.java
+++ basic-thymeleaf/src/main/java/study/thymeleaf/basic/BasicController.java
@@ -5,6 +5,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Controller
 @RequestMapping("/basic")
 public class BasicController {
@@ -19,5 +25,84 @@ public class BasicController {
     public String textUnescaped(Model model) {
         model.addAttribute("data", "Hello <b>Spring!</b>");
         return "basic/text-unescaped";
+    }
+
+    @GetMapping("/variable")
+    public String variable(Model model) {
+        User userA = new User("userA", 10);
+        User userB = new User("userB", 20);
+
+        List<User> list = new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
+
+        Map<String, User> map = new HashMap<>();
+        map.put("userA", userA);
+        map.put("userB", userB);
+
+        model.addAttribute("user", userA);
+        model.addAttribute("users", list);
+        model.addAttribute("userMap", map);
+
+        return "basic/variable";
+    }
+
+
+
+    static class User {
+        private String username;
+        private int age;
+
+        public User(String username, int age) {
+            this.username = username;
+            this.age = age;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+        public void setUsername(String username) {
+            this.username = username;
+        }
+        public int getAge() {
+            return age;
+        }
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return "User(username=" + this.username + ", age=" + this.age + ")";
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == this)
+                return true;
+            if (!(o instanceof User))
+                return false;
+            User user = (User) o;
+            if(!(user.canEqual((Object)this))) return false;
+            if(this.getUsername() == null ?
+                    user.getUsername() != null
+                    : !this.getUsername().equals((user.getUsername())))
+                return false;
+            if(Integer.compare(this.age, user.age) != 0) return false;
+            return true;
+        }
+
+        private boolean canEqual(Object other) {
+            return other instanceof User;
+        }
+
+        @Override
+        public int hashCode() {
+            final int PRIME = 59;
+            int result = 1;
+            result = (result * PRIME) + super.hashCode();
+            result = (result * PRIME) + this.age;
+            return result;
+        }
     }
 }
