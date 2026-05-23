f686e00fb4bd8c204bfd198dbb9e164e73ed79eb
Bruce Han <intragerhan@gmail.com>
2026-05-23 11:50:04 +0900

    오타 수정

--- login/src/test/java/hello/login/web/session/SessionManagerTest.java
+++ login/src/test/java/hello/login/web/session/SessionManagerTest.java
@@ -1,14 +1,11 @@
-기package hello.login.web.session;
+package hello.login.web.session;
 
 import hello.login.domain.member.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
-import javax.servlet.http.HttpServletResponse;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class SessionManagerTest {
     SessionManager sessionManager = new SessionManager();
