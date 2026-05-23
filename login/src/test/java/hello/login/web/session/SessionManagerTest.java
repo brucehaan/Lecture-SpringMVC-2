b62cd457ac3b0144dbbd73bdbfb88df3bc06a110
Bruce Han <intragerhan@gmail.com>
2026-05-23 11:49:01 +0900

    세션 직접 만들기

---
+++ login/src/test/java/hello/login/web/session/SessionManagerTest.java
@@ -0,0 +1,38 @@
+기package hello.login.web.session;
+
+import hello.login.domain.member.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import javax.servlet.http.HttpServletResponse;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SessionManagerTest {
+    SessionManager sessionManager = new SessionManager();
+
+    @Test
+    void sessionTest() {
+        // 세션 생성
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        Member member = new Member();
+        sessionManager.createSession(member, response);
+        // 여기까지는 웹 브라우저로 응답이 나간 거 //
+
+        // 여기부터는 웹 브라우저에서 요청하는 거(웹 브라우저의 요청) //
+        // 요청에 응답 쿠키 저장
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setCookies(response.getCookies());
+
+        // 서버에서 확인해보는 거, 세션 조회
+        Object result = sessionManager.getSession(request);
+        assertThat(result).isEqualTo(member);
+
+        // 세션 만료
+        sessionManager.expire(request);
+        Object expired = sessionManager.getSession(request);
+        assertThat(expired).isNull();
+    }
+}
